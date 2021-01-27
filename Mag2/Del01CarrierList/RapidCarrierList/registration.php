c5fcec86748aec626bba84dafea6b3b222bae127
Nathan MARCY <nmarcy@datasolution.fr>
2021-01-27 11:45:33 +0100

    Create Module Tracking url and Carrier List and create two news vendors

---
+++ Mag2/Del01CarrierList/RapidCarrierList/registration.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * DISCLAIMER
+ *
+ * @category  Del01CarrierList
+ * @package   Del01CarrierList\TrackingUrl
+ * @author    Del01
+ * @copyright 2021 del01
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Del01CarrierList_RapidCarrierList',
+    __DIR__
+);
